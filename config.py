0b9c8286e28af56c01e25c22ad18bbe509b000b2
Andrew Makhnev <andrewmakhnev@gmail.com>
2016-11-17 16:12:37 +0300

    Adding remotly shellcode generator

---
+++ config.py
@@ -0,0 +1,9 @@
+# url for cloud generate shellcode
+CLOUD_SERVER = "http://192.168.1.86:8080"
+
+# Kali with ssh server
+SSH_SERVER = "192.168.1.241"
+SSH_PORT = 22
+SSH_LOGIN = "root"
+SSH_PASSWORD = "toor"
+# Ending Kali ssh server settings
